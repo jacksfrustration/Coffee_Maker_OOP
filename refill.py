969a2b9260fb8b6c410cc37dc9ef0978362a0dab
Georgios Adamidis <85418571+jacksfrustration@users.noreply.github.com>
2024-04-11 14:10:23 +0100

    Add files via upload

---
+++ refill.py
@@ -0,0 +1,8 @@
+class Refiller:
+    def __init__(self):
+        '''refills resources on command'''
+        self.resources={
+            "water":250,
+            "milk":250,
+            "coffee":250
+        }
